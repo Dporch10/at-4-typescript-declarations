163da11d200c46fdbed761feb565c7ab47e84d71
DPorch10 <aintshit@DPorch10s-MacBook-Pro.local>
2024-06-03 22:18:44 -0400

    homework

---
+++ types/index.d.ts
@@ -0,0 +1,5 @@
+declare module 'lodash' {
+    function multiply (num1: number, num2: number): number
+    function reverse <T>(arr: T[]): T[]
+    function dropRight(arr: any[], n: number): any []
+}
